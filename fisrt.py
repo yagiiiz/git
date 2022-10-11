ba0ca2dcbada451ff1a295ce0ded1b303f8b0f9c
yagizuniversity <yyygzwural@gmail.com>
2022-10-11 16:54:30 +0300

    sinek ve kalp cizimi basarili

--- fisrt.py
+++ fisrt.py
@@ -1,1 +1,26 @@
-print('Hello World') #Merhaba Dünya1
+# print('Hello World') #Merhaba Dünya1 
+from turtle import begin_fill, circle, color, end_fill, forward, left, pensize, right
+
+#color(red)
+begin_fill()
+pensize(2)
+left(50)
+forward(133)
+circle(50, 200)
+right(136)
+circle(50, 200)
+forward(133)
+end_fill() 
+
+
+#FLY
+#color(red)
+#begin_fill()
+#pensize(6)
+#left(100)
+#forward(266)
+#circle(100, 400)
+#right(272)
+#circle(100, 400)
+#forward(266)
+#end_fill() 
